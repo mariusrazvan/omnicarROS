f6f16401fea99d0bdbb534fe73567dee350a2b1d
Varvarei Marius Razvan <razvangeniul@gmail.com>
2025-10-18 13:43:25 +0200

    Update CONSTANTS.py

--- read_elrs/read_elrs/CONSTANTS.py
+++ read_elrs/read_elrs/CONSTANTS.py
@@ -7,3 +7,5 @@ motor_rpm_max = 200.0 # 200 rpm max for jgb37-520 motor 12V 200 rpm
 
 max_linear_velocity = (motor_rpm_max / 60) * 2 * math.pi * wheel_radius # in m/s
 max_angular_velocity = (motor_rpm_max / 60) * 2 * math.pi # in rad/s
+
+tof_frame_id = "range_finder"
